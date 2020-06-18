a9a5eedb0d05445c0038ae88052b693f0e2830ab
Radu <rsrusu90@gmail.com>
2020-06-18 19:55:50 +0100

    Updates

---
+++ precache-manifest.db34ed3fc4df2ba4030ef6c4c8e8c3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f351a312ac688a1811d2f96038eef883",
+    "url": "/monster-cards/index.html"
+  },
+  {
+    "revision": "e1a845f0d7c6b70b6c5c",
+    "url": "/monster-cards/static/css/main.3b204df2.chunk.css"
+  },
+  {
+    "revision": "e4f25e371f18ae9db564",
+    "url": "/monster-cards/static/js/2.0e0a43ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cards/static/js/2.0e0a43ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a845f0d7c6b70b6c5c",
+    "url": "/monster-cards/static/js/main.fe008323.chunk.js"
+  },
+  {
+    "revision": "0603b0a5437a87d7932c",
+    "url": "/monster-cards/static/js/runtime-main.6afd87db.js"
+  }
+]);
